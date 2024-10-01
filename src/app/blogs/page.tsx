000d11b9177dcd38f6af251476d7f549a49bdfd0
Karthik_Yeluripati <karthikyeluripati2k@gmail.com>
2024-10-01 13:16:42 -0400

    rerun

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -5,15 +5,15 @@ const HomePage = () => {
   const postMetadata = getPostMetadata();
 
   // Helper function to parse the date string
-  const parseDate = (dateStr) => {
+  const parseDate = (dateStr: string): Date => {
     const [day, month, year] = dateStr.split('-').map(Number);
     // Note: JavaScript months are 0-indexed, so we subtract 1 from the month
     return new Date(2000 + year, month - 1, day);
   };
 
   // Sort the postMetadata array by date in descending order (newest first)
-  const sortedPostMetadata = postMetadata.sort((a, b) => {
-    return parseDate(b.date) - parseDate(a.date);
+  const sortedPostMetadata = [...postMetadata].sort((a, b) => {
+    return parseDate(b.date).getTime() - parseDate(a.date).getTime();
   });
 
   const PostPreviews = sortedPostMetadata.map((post) => (
